9a442f0b6b96e24c97bc8d12d44bb98106c5cec9
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-05-18 11:34:41 -0500

    chore: Remove unused imports and comments, and add new components to App.tsx

--- TrujiReact/src/App.tsx
+++ TrujiReact/src/App.tsx
@@ -1,27 +1,17 @@
 // import { useState } from 'react';
-import './App.css';
+// import './App.css';
 
-import { Button } from 'react-bootstrap';
+// import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import TabsExample from './components/nav';
+import Contact from './components/contact';
 
 function App() {
 
   return (
     <>
-    <h1>TrujiStudios</h1>
-
-    <Button variant="success">Primary</Button>{' '}
-
-    <Button >Prueba</Button>
-
-    <Button className='btn'>Node</Button> 
-
-    <button type="button" className="btn btn-success">Base class</button>
-
-    <Button variant="primary" type="submit">
-        Submit
-      </Button>
-      
+    <TabsExample />
+    <Contact />
     </>
   );
 }
---
+++ TrujiReact/src/components/contact.tsx
@@ -0,0 +1,73 @@
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function Contact() {
+    return (
+        <>
+            <Container className='mt-4'>
+                <Row>
+                    {/* <Col
+                       lg={6}
+                        className='mx-auto'>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                            <Form.Control
+                                placeholder="Username"
+                                aria-label="Username"
+                                aria-describedby="basic-addon1"
+                            />
+                        </InputGroup>
+
+                        <InputGroup className="mb-3">
+                            <Form.Control
+                                placeholder="Recipient's username"
+                                aria-label="Recipient's username"
+                                aria-describedby="basic-addon2"
+                            />
+                            <InputGroup.Text id="basic-addon2">@example.com</InputGroup.Text>
+                        </InputGroup>
+
+                        <Form.Label htmlFor="basic-url">Your vanity URL</Form.Label>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text id="basic-addon3">
+                                https://example.com/users/
+                            </InputGroup.Text>
+                            <Form.Control id="basic-url" aria-describedby="basic-addon3" />
+                        </InputGroup>
+
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text>$</InputGroup.Text>
+                            <Form.Control aria-label="Amount (to the nearest dollar)" />
+                            <InputGroup.Text>.00</InputGroup.Text>
+                        </InputGroup>
+
+                        <InputGroup>
+                            <InputGroup.Text>With textarea</InputGroup.Text>
+                            <Form.Control as="textarea" aria-label="With textarea" />
+                        </InputGroup>
+                    </Col> */}
+
+
+
+
+                    <Col lg={4}>
+                        <h1 className='text-justify text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero tempore dignissimos eos aspernatur excepturi. Unde, itaque harum soluta sint corrupti nisi et, accusantium minus non impedit eos praesentium consectetur nesciunt.</h1>
+                    </Col>
+
+                    <Col lg={4}>
+                        <h1 className='text-justify text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero tempore dignissimos eos aspernatur excepturi. Unde, itaque harum soluta sint corrupti nisi et, accusantium minus non impedit eos praesentium consectetur nesciunt.</h1>
+                    </Col>
+
+                    <Col lg={4}>
+                        <h1 className='text-justify text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero tempore dignissimos eos aspernatur excepturi. Unde, itaque harum soluta sint corrupti nisi et, accusantium minus non impedit eos praesentium consectetur nesciunt.</h1>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
+}
+
+export default Contact;
