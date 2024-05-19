f597a5778391691c9b854a911aaee75f7b57f708
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-05-19 11:09:08 -0500

    chore: Remove unused imports and comments, and update App.tsx with new components

--- TrujiReact/src/main.tsx
+++ TrujiReact/src/main.tsx
@@ -6,15 +6,6 @@ import router from './routes';
 import TabsExample from './components/nav.tsx'
 // import App from './App.tsx'
 // import './index.css'
-;
-
-
-
-
-
-
-
-
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
